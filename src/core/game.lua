6dba90829980232eaa1d7ae43634356385b4ec26
Finn4239 <finn.oettinger@outlook.de>
2025-11-27 17:16:58 +0100

    implemented player movement and simple obstacle detection

--- src/core/game.lua
+++ src/core/game.lua
@@ -1,13 +1,43 @@
+-- START-Coordinates
+x = 0
+y = 40
+-- constants
+PLAYER_SPRITE = 1
+BOTTOM_SPRITE = 4
 
-X = 64  Y = 64
-function _UPDATE()
-  IF (BTN(0)) THEN X=X-1 END
-  IF (BTN(1)) THEN X=X+1 END
-  IF (BTN(2)) THEN Y=Y-1 END
-  IF (BTN(3)) THEN Y=Y+1 END
-END
+function _draw()
+    cls(5)
 
-FUNCTION _DRAW()
-  CLS(5)
-  CIRCFILL(X,Y,7,14)
-END
+    map()
+    spr(PLAYER_SPRITE, x, y)
+    print("x="..x.." y="..y, 0, 0, 7) --debug output
+end
+
+function _update()
+    -- Save player coordinates
+    local lx = x
+    local ly = y
+
+    if btn(0) then x = x - 1 end -- move left
+    if btn(1) then x = x + 1 end -- move right
+    if btn(2) then y = y - 1 end --move down
+    if btn(3) then y = y + 1 end -- move up
+
+    if isObstacle() then
+        x = lx
+        y = ly
+    end
+end
+
+function isObstacle()
+    local tx = flr((x+7) / 8)
+    local ty = flr((y+7) / 8)
+
+    return fget(mget(tx, ty), 0)
+end
+
+function gravity()
+
+end
+
+
