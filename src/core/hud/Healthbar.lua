c9dea32fcb7a8fb9e0e59446bf662a7b73e56235
Finn4239 <finn.oettinger@outlook.de>
2026-01-29 01:06:51 +0100

    added Healthbar.lua

--- src/core/hud/Healthbar.lua
+++ src/core/hud/Healthbar.lua
@@ -1,12 +1,12 @@
 function draw_health_bar(hp, max_hp, x, y, w, h)
     -- def. der farben
-    col_bg = 6 -- (weiß)
+    col_bg = 7 -- (weiß)
     col_fg = 8 -- (rot)
 
     -- rechteck
     rectfill(x, y, x+w, y+h, col_bg)
 
-    -- einteilen des lebensbalkens in blれへcke
+    -- einteilen des lebensbalkens in blöcke
     -- wenn < 0 -> spieler hat kein leben mehr, ratio nicht weiter berechnen sonst negativ
     -- wenn > 1 -> spieler hat noch volles leben, ratio nicht weiter berechnen da sonst mehr als healthbar erlaubt
     local ratio = hp / max_hp
